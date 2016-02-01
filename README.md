8bd87704627598095407cb75fecf88e5f00784c9
Fauve <contact@taniere.info>
2016-02-01 13:08:47 +0000

    Commenting code.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 SH script to copy artist tag to empty album_artist tag in music file format
 
 Some music files havent the `ALBUMARTIST` tag set. So, in iTunes-like players when the left column is sorted by `ALBUMARTIST` many track are sorted in the `<empty>` section for unknown `ALBUMARTIST`. This is mainly usefull for collection with various artist albums, like OST albums.
+
+It work only for **FLAC** and **mp3**, for the moment.
+
+# Requierment
+* metaflac
+* id3v2
--- addalbumartisttag.sh
+++ addalbumartisttag.sh
@@ -1,5 +1,6 @@
 #!/bin/env sh
 
+# Deffinition of the working directory, the curren one in any.
 if [ $# -eq 0 ]; then
 	DIR=.
 else
@@ -9,39 +10,38 @@ fi
 # Generating lists of files wich will be processed
 find $DIR -type f -name "*.flac" > temporary-flac-list-file
 find $DIR -type f -name "*.mp3"  > temporary-mp3-list-file
-find $DIR -type f -name "*.ogg"  > temporary-ogg-list-file
-
 
 # Processing flac files
+echo "* Processing FLAC files"
 while read g; do
 	echo "Processing \"$g\""
-	metaflac --export-tags-to="$g"-metatag-temp "$g"
-	if grep --quiet --ignore-case "^ALBUMARTIST=[^$]" "$g"-metatag-temp ; then
+	metaflac --export-tags-to="$g"-metatag-temp "$g" # Creation of a temporary metainformation file.
+	if grep --quiet --ignore-case "^ALBUMARTIST=[^$]" "$g"-metatag-temp ; then # Evaluating if the ALBUMARTIST field is empty.
 		echo "There is noting to do for this file."
 	else
 		echo "An “ALBUMARTIST” will be add."
-		metaflac --remove-tag=ALBUMARTIST "$g"
-		#metaflac --set-tag="ALBUMARTIST=`more "$g"-metatag-temp | grep --no-message --ignore-case --regexp='^ARTIST' | sed 's/^ARTIST=//'`" "$g"
+		metaflac --remove-tag=ALBUMARTIST "$g" # Purging the ALBUMARTIST field before set it.
 		metaflac --set-tag="ALBUMARTIST=`cat "$g"-metatag-temp | grep --no-message --ignore-case --regexp='^ARTIST' | sed 's/^ARTIST=//'`" "$g"
 	fi
-	rm "$g"-metatag-temp
+	rm "$g"-metatag-temp # Deleting the temporary metainformation file.
 done < temporary-flac-list-file
 
-#rm temporary-flac-list-file
+rm temporary-flac-list-file # Deleting the FLAC files list.
 
 
 # Processing mp3 files
+echo "* Processing mp3 files"
 while read g; do
 	echo "Processing \"$g\""
-	ALBUMARTISTTAG=`id3v2 -l "$g" | grep TPE2 | sed 's/TPE2 (Band\/orchestra\/accompaniment): //'`
-	ARTISTTAG=`id3v2 -l "$g" | grep TPE1 | sed 's/TPE1 (Lead performer(s)\/Soloist(s)): //'`
+	ALBUMARTISTTAG=`id3v2 -l "$g" | grep TPE2 | sed 's/TPE2 (Band\/orchestra\/accompaniment): //'` # Catching the ALBUMARTIST tag before processing the file.
+	ARTISTTAG=`id3v2 -l "$g" | grep TPE1 | sed 's/TPE1 (Lead performer(s)\/Soloist(s)): //'` # Catching the ARTIST tag before processing the file.
 	echo $ARTISTTAG
-	if [[ -z "$ALBUMARTISTTAG" ]] ; then
-		echo "An “ALBUMARTIST” will be add."
+	if [[ -z "$ALBUMARTISTTAG" ]] ; then # Evaluating if ALBUMARTIST field is empty.
+		echo "An “ALBUMARTIST” will be add." # If yes, the ALBUMARTIST field take the content of the ARTIST tag.
 		id3v2 --TPE2 "$ARTISTTAG" "$g"
 	else
 		echo "There is noting to do for this file."
 	fi
 done < temporary-mp3-list-file
 
-#rm temporary-mp3-list-file
+rm temporary-mp3-list-file # Deleting the mp3 files list.
