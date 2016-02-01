a4b63e4ff6cdc6424b45027de36bd8c0ba7103ee
Fauve <contact@taniere.info>
2016-02-01 01:27:02 +0000

    Processing mp3 files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # addalbumartisttag.sh
 SH script to copy artist tag to empty album_artist tag in music file format
 
-Some music files havent the `ALBUMARTIST` tag set. So, in iTunes-like players when the left column is sorted by `ALBUMARTIST` many track are sorted in the `<empty>` section for unknown `ALBUMARTIST`.
+Some music files havent the `ALBUMARTIST` tag set. So, in iTunes-like players when the left column is sorted by `ALBUMARTIST` many track are sorted in the `<empty>` section for unknown `ALBUMARTIST`. This is mainly usefull for collection with various artist albums, like OST albums.
--- addalbumartisttag.sh
+++ addalbumartisttag.sh
@@ -6,22 +6,42 @@ else
 	DIR=$1
 fi
 
-find $DIR -type f -name "*.flac"  > temporary-list-file
+# Generating lists of files wich will be processed
+find $DIR -type f -name "*.flac" > temporary-flac-list-file
+find $DIR -type f -name "*.mp3"  > temporary-mp3-list-file
+find $DIR -type f -name "*.ogg"  > temporary-ogg-list-file
 
 
+# Processing flac files
 while read g; do
-	ls "$g"
 	echo "Processing \"$g\""
 	metaflac --export-tags-to="$g"-metatag-temp "$g"
-	if grep -q --ignore-case "^ALBUMARTIST=[^$]" "$g"-metatag-temp ; then
+	if grep --quiet --ignore-case "^ALBUMARTIST=[^$]" "$g"-metatag-temp ; then
 		echo "There is noting to do for this file."
 	else
-		echo "An “ALBUMARTIST” will be add.\n"
-		metaflac --set-tag="ALBUMARTIST=`more "$g"-metatag-temp | grep --ignore-case -e '^ARTIST' | sed 's/^ARTIST=//'`" "$g"
+		echo "An “ALBUMARTIST” will be add."
+		metaflac --remove-tag=ALBUMARTIST "$g"
+		#metaflac --set-tag="ALBUMARTIST=`more "$g"-metatag-temp | grep --no-message --ignore-case --regexp='^ARTIST' | sed 's/^ARTIST=//'`" "$g"
+		metaflac --set-tag="ALBUMARTIST=`cat "$g"-metatag-temp | grep --no-message --ignore-case --regexp='^ARTIST' | sed 's/^ARTIST=//'`" "$g"
 	fi
-
 	rm "$g"-metatag-temp
-	ls "$g"
-done < temporary-list-file
+done < temporary-flac-list-file
 
-rm temporary-list-file
+#rm temporary-flac-list-file
+
+
+# Processing mp3 files
+while read g; do
+	echo "Processing \"$g\""
+	ALBUMARTISTTAG=`id3v2 -l "$g" | grep TPE2 | sed 's/TPE2 (Band\/orchestra\/accompaniment): //'`
+	ARTISTTAG=`id3v2 -l "$g" | grep TPE1 | sed 's/TPE1 (Lead performer(s)\/Soloist(s)): //'`
+	echo $ARTISTTAG
+	if [[ -z "$ALBUMARTISTTAG" ]] ; then
+		echo "An “ALBUMARTIST” will be add."
+		id3v2 --TPE2 "$ARTISTTAG" "$g"
+	else
+		echo "There is noting to do for this file."
+	fi
+done < temporary-mp3-list-file
+
+#rm temporary-mp3-list-file
